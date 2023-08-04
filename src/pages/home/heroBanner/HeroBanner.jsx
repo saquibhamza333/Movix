0816955b2aa07a41e31413734cd9d2e8455ca42a
saquibhamza333 <worlddream666@gmail.com>
2023-08-04 10:09:02 +0530

    second commit

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import { useNavigate } from "react-router-dom";
+
+import useFetch from "../../../hooks/useFetch";
+
+import './style.scss'
+
+const HeroBanner = () => {
+const [background,setBackground]=useState('');
+const [query, setQuery] = useState("");
+const navigate = useNavigate();
+
+const { data, loading } = useFetch("/movie/upcoming");
+
+   const searchQueryHandler = (event) => {
+        if (event.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+        }
+    };
+
+
+  return (
+    <div className='heroBanner'>
+    <div className='wrapper'>
+      <div className='heroBannerContent'>
+      <span className="title">Welcome.</span>
+                    <span className="subTitle">
+                        Millions of movies, TV shows and people to discover.
+                        Explore now.
+                    </span>
+
+                <div className="searchInput">
+                  <input type="text" placeholder="Search for a movie or tv show...."
+                  onChange={(e) => setQuery(e.target.value)}
+                            onKeyUp={searchQueryHandler}
+                            />
+
+                </div> 
+
+                <button>Search</button>    
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default HeroBanner
